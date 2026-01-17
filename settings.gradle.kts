6dae2b21535e5769f43d45afb50e37d2b8175efa
dexmp <matveymp@yandex.ru>
2026-01-17 20:27:22 +0300

    Правки по дизайну и распределение

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,11 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        // Зеркала вместо заблокированных репозиториев
+        maven { url = uri("https://mirrors.tencent.com/nexus/repository/maven-public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        // Запасные варианты
         google()
         mavenCentral()
     }
